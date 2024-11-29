d1b70197c5c5ff26111fbbf456e65b2b47fa0abe
220110430 <220110430@comp6.hwccluster.labcenter>
2024-11-29 13:37:17 +0000

    lab5.1

--- fs/demo/src/main.c
+++ fs/demo/src/main.c
@@ -37,16 +37,19 @@ struct demo_super super;
 static void* demo_mount(struct fuse_conn_info * conn_info){
     // 打开驱动
     char device_path[128] = {0};
-    sprintf(device_path, "%s/" DEVICE_NAME, getpwuid(getuid())->pw_dir);
-    super.driver_fd = ddriver_open(device_path);
+    sprintf(device_path, "%s/" DEVICE_NAME, getpwuid(getuid())->pw_dir); // 输入东西到device_path
+    super.driver_fd = ddriver_open(device_path);    
 
     printf("super.driver_fd: %d\n", super.driver_fd);
-
-
+    int sz_io = 0;
+    int sz_disk = 0;
+    // 通过ddriver_ioctl来获取磁盘容量和IO块大小等信息。 根据传入命令cmd获取虚拟磁盘信息。cmd为IOC_REQ_DEVICE_SIZE，void *ret处返回虚拟磁盘总大小。cmd为IOC_REQ_DEVICE_IO_SZ，void *ret参数处返回虚拟磁盘的IO大小。
+    ddriver_ioctl(super.driver_fd, IOC_REQ_DEVICE_IO_SZ, &sz_io);
+    ddriver_ioctl(super.driver_fd, IOC_REQ_DEVICE_SIZE, &sz_disk);
     /* 填充super信息 */
-    super.sz_io = /* TODO */;
-    super.sz_disk = /* TODO */;
-    super.sz_blks = /* TODO */; 
+    super.sz_io = sz_io;
+    super.sz_disk = sz_disk;
+    super.sz_blks = sz_io << 1;  //一个逻辑块是两个IO块大小
 
     return 0;
 }
@@ -67,13 +70,18 @@ static int demo_readdir(const char* path, void* buf, fuse_fill_dir_t filler, off
     /* 根据超级块的信息，从第500逻辑块读取一个dentry，ls将只固定显示这个文件名 */
 
     /* TODO: 计算磁盘偏移off，并根据磁盘偏移off调用ddriver_seek移动磁盘头到磁盘偏移off处 */
+    off_t _offset = 500 * super.sz_blks;
+    ddriver_seek(super.driver_fd, _offset, SEEK_SET);
 
     /* TODO: 调用ddriver_read读出一个磁盘块到内存，512B */
+    char _buf[512];
+    ddriver_read(super.driver_fd, _buf, 512);
 
     /* TODO: 使用memcpy拷贝上述512B的前sizeof(demo_dentry)字节构建一个demo_dentry结构 */
-
+    struct demo_dentry dentry;
+    memcpy(&dentry, _buf, sizeof(struct demo_dentry));
     /* TODO: 填充filename */
-
+    strcpy(filename, dentry.fname);
     // 此处大家先不关注filler，已经帮同学写好，同学填充好filename即可
     return filler(buf, filename, NULL, 0);
 }
@@ -84,7 +92,7 @@ static int demo_getattr(const char* path, struct stat *stbuf)
     if(strcmp(path, "/") == 0)
         stbuf->st_mode = DEMO_DEFAULT_PERM | S_IFDIR;            // 根目录是目录文件
     else
-        stbuf->st_mode = /* TODO: 显示为普通文件 */;            // 该文件显示为普通文件
+        stbuf->st_mode = DEMO_DEFAULT_PERM | S_IFREG;            // 该文件显示为普通文件
     return 0;
 }
 
